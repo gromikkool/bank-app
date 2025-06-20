66f52fc94154d4afc9f42d180a0d340452975145
rkashko <rusoul98@gmail.com>
2025-06-21 00:20:15 +0200

    person-service initial

---
+++ person-service/src/test/java/com/k3sh/personservice/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k3sh.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+     @Test
+     void contextLoads() {
+     }
+
+}
